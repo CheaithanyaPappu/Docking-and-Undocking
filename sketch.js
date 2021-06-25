821edc0ee31ae7dc95a6aa3e6942e198f238df0c
CheaithanyaPappu <73804385+CheaithanyaPappu@users.noreply.github.com>
2021-06-25 11:45:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var issing, spacebging, space1, space2, space3, space4
+var iss, spacecraft
+var hasDocked = false;
+
+
+function preload() {
+issing = loadImage ("Images/iss.png");
+spacebging = loadImage ("Images/spacebg.jpg");
+space1 = loadImage ("Images/spacecraft1.png");
+space2 = loadImage ("Images/spacecraft2.png");
+space3 = loadImage ("Images/spacecraft3.png");
+space4 = loadImage ("Images/spacecraft4.png");
+  
+  
+}
+
+
+function setup() {
+  createCanvas(displayWidth-20,displayHeight-120);
+
+  spacecraft = createSprite (300,500);
+  spacecraft.addImage (space1);
+  spacecraft.scale = 0.15;
+  
+iss = createSprite (300,300);
+iss.addImage (issing);
+
+
+}
+
+function draw() {
+  background(spacebging); 
+  
+  if(!hasDocked) {
+spacecraft.x = spacecraft.x + random (1,-1);
+  
+
+  if(keyDown("UP_ARROW")) {
+spacecraft.y = spacecraft.y-2;
+  }
+
+if(keyDown("LEFT_ARRROW")) {
+spacecraft.addImage(space3);
+spacecraft.x = spacecraft.x -1;
+}
+
+if (keyDown("RIGHT_ARROW")) {
+spacecraft.addImage(space2);
+spcaecrafy.x = spacecraftx +1;
+
+if(keyDown("DOWN_ARROW")) {
+spacecraft.addImage (space1);
+
+}
+if (spacecrafy.y<=(iss.y+70)&& spacecraft.x <= (iss,x-10)) {
+hasDocked = true;
+textSize (25);
+fill("white");
+text("Docking Successfull!", 200,300);
+
+
+}
+
+
+}
+
+
+
+
+
+
+  }
+  }
+
+
+  drawSprites();
